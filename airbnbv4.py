54189c94551db428e36b16a18dbaf07233be947f
Vrushank Gude <43715994+vrush0022@users.noreply.github.com>
2019-05-14 18:23:13 +0530

    Update airbnbv4.py

--- airbnbv4.py
+++ airbnbv4.py
@@ -205,64 +205,6 @@ def plotFeatureImportances(model,columns):
     
     seaborn.barplot(y=labels,x=vals)
     
-#One time use function
-def scrapeUpdatedPricesFromAirbnb():
-    import pandas
-    import numpy
-    from bs4 import BeautifulSoup
-    from selenium import webdriver
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-    from selenium.webdriver.chrome.options import Options 
-    import collections
-    '''read processed data'''
-    london_data=pandas.read_csv('C:\\Users\\gudea\\Desktop\\Python\\airbnb\\processed_data.csv')
-    
-    '''scrape'''
-    dollar_rate=70.38
-    chrome_options = Options()  
-    chrome_options.add_argument("--headless")  
-    new_prices=pandas.DataFrame(data=None,columns=['listing_url','new_price'])
-    capa = DesiredCapabilities.CHROME
-    capa["pageLoadStrategy"] = "none"
-                
-    index=0
-    '''change below condition according to what you want to scrape'''    
-    subset=london_data.loc[(london_data.price<40) & (london_data.bedrooms==5),: ]
-    
-    for url in subset.listing_url:          
-        try:            
-            driver = webdriver.Chrome('C:\\Users\\gudea\\Desktop\\Python\\airbnb\\chromedriver_win32\\chromedriver.exe',desired_capabilities=capa,chrome_options=chrome_options)
-            driver.get(url)                
-            wait = WebDriverWait(driver, 10)#10 seconds wait.increase if your net is slow    
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, '_doc79r')))
-            
-            driver.execute_script("window.stop();")        
-            source=driver.page_source
-            soup=BeautifulSoup(source,'html.parser')
-            spandiv=soup.find_all('span',attrs={'class':'_doc79r'}) 
-            if(spandiv!=None and len(spandiv)>0):
-                price=spandiv[0].text
-                price=price.replace(',','').replace('₹','')
-                price=int(price)
-                price=numpy.ceil(price/dollar_rate)
-                new_prices.loc[index,:]=(url,price)
-                index+=1                    
-                driver.close()
-            else:
-                driver.close()
-        except:
-            print('Timeout exception:',url)
-            new_prices.loc[index,:]=(url,'NaN')
-            index+=1
-            driver.close()
-        print(index)
-    
-    '''write file.add ur name to the file'''
-    new_prices.to_csv('scrapped_file.csv',index=False)
-    
 
 def generateInsight(model,features,data):
     pdp_airbnb = pdp.pdp_isolate(model=model,
